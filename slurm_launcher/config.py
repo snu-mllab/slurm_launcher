0547f312d96d0c934e530dcf39eb24346918bc7a
mountguy <riman314@snu.ac.kr>
2022-04-14 19:12:40 +0900

    mnist tutorial

--- slurm_launcher/config.py
+++ slurm_launcher/config.py
@@ -1,6 +1,6 @@
 PARTITION2PYTHON = {
-    'dept' : '~/anaconda3/envs/pt1/bin/python',
-    'titan' : '~/anaconda3/envs/pt2/bin/python',
-    'rtx2080' : '~/anaconda3/envs/pt3/bin/python',
-    'rtx3090' : '~/anaconda3/envs/pt4/bin/python',
+    'dept' : '~/anaconda3/envs/SAC/bin/python',
+    'titan' : '~/anaconda3/envs/SAC/bin/python',
+    'rtx2080' : '~/anaconda3/envs/SAC/bin/python',
+    'rtx3090' : '~/anaconda3/envs/SAC3090/bin/python',
 }
