b4ca3544646f7705fbc5d74bf0eeb2879c9fbf7f
mountguy <riman314@snu.ac.kr>
2022-04-14 19:19:52 +0900

    delete

--- slurm_launcher/config.py
+++
@@ -1,6 +0,0 @@
-PARTITION2PYTHON = {
-    'dept' : '~/anaconda3/envs/SAC/bin/python',
-    'titan' : '~/anaconda3/envs/SAC/bin/python',
-    'rtx2080' : '~/anaconda3/envs/SAC/bin/python',
-    'rtx3090' : '~/anaconda3/envs/SAC3090/bin/python',
-}
