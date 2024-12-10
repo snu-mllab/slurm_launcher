26490842b0629f26d44ca3bdea299973a6c4c1b3
Junyoung Yeom <yeomjy@mllab.snu.ac.kr>
2024-12-10 17:58:54 +0900

    update: jupyter sbatch script

--- examples/jupyter.sh
+++ examples/jupyter.sh
@@ -23,8 +23,9 @@ unset XDG_RUNTIME_DIR
 date;pwd
 
 host=$(hostname)
-ip=$(ifconfig | grep 147 | awk '{print $2}')
-port=$(ruby -e 'require "socket"; puts Addrinfo.tcp("", 0).bind {|s| s.local_address.ip_port }')
+port=$(/usr/bin/python3 -c "import socket;sock=socket.socket();sock.bind(('', 0));print(sock.getsockname()[1])")
+ip=$(/usr/bin/python3 -c "import socket; print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith(\"127.\")][:1], [[(s.connect((\"8.8.8.8\", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])")
+
 echo ${host}
 echo ${ip}
 echo ${port}
